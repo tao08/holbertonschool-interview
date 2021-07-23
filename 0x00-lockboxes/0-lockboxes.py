6517961a99cc4c37118e85de5b800cfcc9b00069
tao08 <1823@holbertonschool.com>
2021-07-23 06:31:26 -0500

    change the code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,11 +5,14 @@ Method that determines if all the boxes can be opened.
 
 
 def canUnlockAll(boxes):
-    num_of_boxes = len(boxes)
-    list_of_keys = boxes[0]
-    box_locked = [False] + [True] * (num_of_boxes - 1)
-    for key in list_of_keys:
-        if ((key < num_of_boxes) and (box_locked[key] is True)):
-            box_locked[key] = False
-            list_of_keys.extend(boxes[key])
-    return not any(box_locked)
+    unlock_boxes = 0
+    unlock = [unlock_boxes]
+    for index in unlock:
+        underbox = boxes[index]
+        for indexKeyBox in underbox:
+            if indexKeyBox not in unlock and indexKeyBox < len(boxes):
+                unlock += [indexKeyBox]
+    if len(unlock) == len(boxes):
+        return True
+    else:
+        return False
